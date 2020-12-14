0dd2b16d5f1b9c90fc8be767cdb546982e7e498b
faci2000 <faci20000@gmail.com>
2020-12-14 01:34:36 +0100

    Added displaying map with jungle using javafx

--- src/EvolutionGenerator/main/Map/Map.java
+++ src/EvolutionGenerator/main/Map/Map.java
@@ -7,26 +7,30 @@ import MapElements.Position;
 import java.util.*;
 
 public class Map {
-    HashMap<Position, SortedSet<Animal>> animalsSet;
-    HashMap<Position, Grass> grassesSet;
-    int mapWidth;
-    int mapHeight;
-    int jungleWidth;
-    int jungleHeight;
-    Position jungleOffset;
-    int mapId;
+    private HashMap<Position, SortedSet<Animal>> animalsSet;
+    private HashMap<Position, Grass> grassesSet;
+    private int mapWidth;
+    private int mapHeight;
+    private int jungleWidth;
+    private int jungleHeight;
+    private Position jungleOffset;
+    private int mapId;
 
     public Map(int mapWidth,int mapHeight,int jungleWidth,int jungleHeight,int mapId){
-        this.mapWidth = mapWidth;
-        this.mapHeight = mapHeight;
-        this.jungleWidth = jungleWidth;
-        this.jungleHeight = jungleHeight;
-        this.mapId = mapId;
-        jungleOffset=new Position((mapWidth-jungleWidth)/2,(mapHeight-jungleHeight)/2);
+        this.setMapWidth(mapWidth);
+        this.setMapHeight(mapHeight);
+        this.setJungleWidth(jungleWidth);
+        this.setJungleHeight(jungleHeight);
+        this.setMapId(mapId);
+        setJungleOffset(new Position((mapWidth-jungleWidth)/2,(mapHeight-jungleHeight)/2));
+    }
+
+    public boolean isInJungle(int x, int y){
+        return ((jungleOffset.getX()<=x)&&((jungleOffset.getX()+ getJungleWidth() -1)>=x)&&(jungleOffset.getY()<=y)&&((jungleOffset.getY()+ getJungleHeight() -1)>=y));
     }
 
     public void addAnimalToAnimalsSet(Animal animal){
-        addAnimalTo(this.animalsSet,animal);
+        addAnimalTo(this.getAnimalsSet(),animal);
     }
 
     public void addAnimalTo(HashMap<Position, SortedSet<Animal>> animalsSet,Animal animal){
@@ -36,7 +40,7 @@ public class Map {
     }
 
     public Animal removeAnimalFromAnimalsSet(Animal animal){
-        return removeAnimalFrom(this.animalsSet,animal);
+        return removeAnimalFrom(this.getAnimalsSet(),animal);
     }
 
     public Animal removeAnimalFrom(HashMap<Position, SortedSet<Animal>> animalsSet,Animal animal){
@@ -47,35 +51,32 @@ public class Map {
     }
 
     public Grass removeGrassFromGrassSet(Position position){
-        return grassesSet.remove(position);
+        return getGrassesSet().remove(position);
     }
 
     private boolean isGrassOn(Position position){
-        return this.grassesSet.containsKey(position);
+        return this.getGrassesSet().containsKey(position);
     }
 
     public void growGrassesOnMap(){
         for(int i=0;i<2;i++){
             Position newGrassPosition;
             do{
-                newGrassPosition = Position.generateRandomPositionInRangeWithExcludedScope(mapWidth,mapHeight,jungleWidth,jungleHeight,jungleOffset);
+                newGrassPosition = Position.generateRandomPositionInRangeWithExcludedScope(getMapWidth(), getMapHeight(), getJungleWidth(), getJungleHeight(), getJungleOffset());
             }while(!isGrassOn(newGrassPosition));
-            grassesSet.put(new Position(newGrassPosition),new Grass(newGrassPosition,mapId));
+            getGrassesSet().put(new Position(newGrassPosition),new Grass(newGrassPosition, getMapId()));
             do{
-                newGrassPosition=Position.generateRandomPositionInRange(jungleOffset.getX(),jungleOffset.getY(),jungleOffset.getX()+jungleWidth-1,jungleOffset.getY()+jungleHeight-1);
+                newGrassPosition=Position.generateRandomPositionInRange(getJungleOffset().getX(), getJungleOffset().getY(), getJungleOffset().getX()+ getJungleWidth() -1, getJungleOffset().getY()+ getJungleHeight() -1);
             }while(!isGrassOn(newGrassPosition));
-            grassesSet.put(new Position(newGrassPosition),new Grass(newGrassPosition,mapId));
+            getGrassesSet().put(new Position(newGrassPosition),new Grass(newGrassPosition, getMapId()));
         }
     }
 
     public ArrayList<Animal> collectDeadAnimals(){
         ArrayList<Animal> deadAnimals = new ArrayList<>();
-        Iterator<Position> positionIterator = animalsSet.keySet().iterator();
-        while(positionIterator.hasNext()){
-            Iterator<Animal> animalIterator = animalsSet.get(positionIterator.next()).iterator();
-            while (animalIterator.hasNext()){
-                Animal animal = animalIterator.next();
-                if(animal.isDead()){                        //possible problem with deleting set, before end of iterating
+        for (Position position : getAnimalsSet().keySet()) {
+            for (Animal animal : getAnimalsSet().get(position)) {
+                if (animal.isDead()) {                        //possible problem with deleting set, before end of iterating
                     deadAnimals.add(animal);
                     removeAnimalFromAnimalsSet(animal);
                 }
@@ -87,60 +88,120 @@ public class Map {
 
     public void rotateAndMoveEachAnimal(){
         HashMap<Position, SortedSet<Animal>> refreshedAnimalsSet = new HashMap<>();
-        Iterator<Position> positionIterator = animalsSet.keySet().iterator();
-        while(positionIterator.hasNext()) {
-            Iterator<Animal> animalIterator = animalsSet.get(positionIterator.next()).iterator();
-            while (animalIterator.hasNext()) {
-                Animal animal = animalIterator.next();
+        for (Position position : getAnimalsSet().keySet()) {
+            for (Animal animal : getAnimalsSet().get(position)) {
                 removeAnimalFromAnimalsSet(animal);
-                animal.moveYourself(mapHeight,mapWidth);
-                addAnimalTo(refreshedAnimalsSet,animal);
+                animal.moveYourself(getMapHeight(), getMapWidth());
+                addAnimalTo(refreshedAnimalsSet, animal);
             }
         }
-        animalsSet=refreshedAnimalsSet;
+        setAnimalsSet(refreshedAnimalsSet);
     }
 
     public boolean hasMoreItemsThan(HashMap<?,?> hashMap){
-        return animalsSet.size()>hashMap.size();
+        return getAnimalsSet().size()>hashMap.size();
     }
 
     public void feedAnimals(){
-        if(this.hasMoreItemsThan(grassesSet))               // hasMoreItemsThan() is only to make almost whole sentence from java code ;)
+        if(this.hasMoreItemsThan(getGrassesSet()))               // hasMoreItemsThan() is only to make almost whole sentence from java code ;)
             feedAnimalsIteratingOverGrassesSet();
         else
             feedAnimalsIteratingOverAnimalsSet();
     }
 
     public void feedAnimalsIteratingOverGrassesSet(){
-        Iterator<Position> grassesPositionIterator = grassesSet.keySet().iterator();
-        feedAnimalsUsing(grassesPositionIterator,grassesSet);
+        Iterator<Position> grassesPositionIterator = getGrassesSet().keySet().iterator();
+        feedAnimalsUsing(grassesPositionIterator, getGrassesSet());
     }
 
     public void feedAnimalsIteratingOverAnimalsSet(){
-        Iterator<Position> animalPositionIterator = animalsSet.keySet().iterator();
-        feedAnimalsUsing(animalPositionIterator,animalsSet);
+        Iterator<Position> animalPositionIterator = getAnimalsSet().keySet().iterator();
+        feedAnimalsUsing(animalPositionIterator, getAnimalsSet());
     }
 
     public void feedAnimalsUsing(Iterator<Position> positionIterator,HashMap<Position,?> positionHashMap){
         while(positionIterator.hasNext()){
             Position checkPosition = positionIterator.next();
             if(positionHashMap.containsKey(checkPosition)){
-                animalsSet.get(checkPosition).first().eat();
+                getAnimalsSet().get(checkPosition).first().eat();
                 removeGrassFromGrassSet(checkPosition);
             }
         }
     }
 
     public void reproduceAnimals(){
-        Iterator<Position> positionIterator = animalsSet.keySet().iterator();
+        Iterator<Position> positionIterator = getAnimalsSet().keySet().iterator();
         while(positionIterator.hasNext()) {
             Position animalsPlace = positionIterator.next();
-            if(animalsSet.get(animalsPlace).size()>1){
-                Animal newBornAnimal = animalsSet.get(animalsPlace).first().copulateWith(((Animal)animalsSet.get(animalsPlace).toArray()[1]));
+            if(getAnimalsSet().get(animalsPlace).size()>1){
+                Animal newBornAnimal = getAnimalsSet().get(animalsPlace).first().copulateWith(((Animal) getAnimalsSet().get(animalsPlace).toArray()[1]));
                 addAnimalToAnimalsSet(newBornAnimal);
             }
         }
     }
 
 
+    public HashMap<Position, SortedSet<Animal>> getAnimalsSet() {
+        return animalsSet;
+    }
+
+    public void setAnimalsSet(HashMap<Position, SortedSet<Animal>> animalsSet) {
+        this.animalsSet = animalsSet;
+    }
+
+    public HashMap<Position, Grass> getGrassesSet() {
+        return grassesSet;
+    }
+
+    public void setGrassesSet(HashMap<Position, Grass> grassesSet) {
+        this.grassesSet = grassesSet;
+    }
+
+    public int getMapWidth() {
+        return mapWidth;
+    }
+
+    public void setMapWidth(int mapWidth) {
+        this.mapWidth = mapWidth;
+    }
+
+    public int getMapHeight() {
+        return mapHeight;
+    }
+
+    public void setMapHeight(int mapHeight) {
+        this.mapHeight = mapHeight;
+    }
+
+    public int getJungleWidth() {
+        return jungleWidth;
+    }
+
+    public void setJungleWidth(int jungleWidth) {
+        this.jungleWidth = jungleWidth;
+    }
+
+    public int getJungleHeight() {
+        return jungleHeight;
+    }
+
+    public void setJungleHeight(int jungleHeight) {
+        this.jungleHeight = jungleHeight;
+    }
+
+    public Position getJungleOffset() {
+        return jungleOffset;
+    }
+
+    public void setJungleOffset(Position jungleOffset) {
+        this.jungleOffset = jungleOffset;
+    }
+
+    public int getMapId() {
+        return mapId;
+    }
+
+    public void setMapId(int mapId) {
+        this.mapId = mapId;
+    }
 }
--- src/EvolutionGenerator/main/UserInterface/AppWindow.java
+++ src/EvolutionGenerator/main/UserInterface/AppWindow.java
@@ -1,4 +1,61 @@
 package UserInterface;
+import Map.Map;
+import com.sun.tools.javac.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-public class AppWindow {
+import java.net.URL;
+
+public class AppWindow extends Application {
+
+    Button button;
+
+    @Override
+    public void start( Stage primaryStage) throws Exception {
+        primaryStage.setTitle("EvolutionGenerator");
+       button=new Button();
+       button.setText("ClickMe");
+
+
+
+        VBox layout = new VBox();
+        Scene scene = new Scene(layout,720,720);
+        layout.getChildren().add(displayMap(new Map(80,100,20,50,0),scene.getWidth(),scene.getHeight()));
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public GridPane displayMap(Map map,double sceneWidth, double sceneHeight){
+        Group[][] field = new Group[map.getMapWidth()][map.getMapHeight()];
+        GridPane mapRepresentation = new GridPane();
+        mapRepresentation.setHgap(sceneHeight/map.getMapHeight()*0.2);
+        mapRepresentation.setVgap(sceneWidth/map.getMapWidth()*0.2);
+        for(int i=0;i< map.getMapWidth();i++){
+            for (int j=0;j< map.getMapHeight();j++){
+                field[i][j]=new Group(new Rectangle(0,0,(sceneWidth-map.getMapWidth())/map.getMapWidth(), (sceneHeight-map.getMapHeight())/ map.getMapHeight()));
+                if(map.isInJungle(i,j))
+                    ((Rectangle)field[i][j].getChildren().get(0)).setFill(Color.DARKGREEN);
+                else
+                    ((Rectangle)field[i][j].getChildren().get(0)).setFill(Color.GREEN);
+                mapRepresentation.add(field[i][j],i,j);
+            }
+        }
+        return mapRepresentation;
+
+    }
 }
